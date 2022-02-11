412fc089acb231ea26659d4558470e5da58399c7
Carlos Diaz <carlos.cedp@gmail.com>
2022-02-10 22:44:07 -0400

    domain layer added

--- RecruitmentSystem.UI/Controllers/HomeController.cs
+++ RecruitmentSystem.UI/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using RecruitmentSystem.UI.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -11,27 +10,13 @@ namespace RecruitmentSystem.UI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
-        }
+}
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
