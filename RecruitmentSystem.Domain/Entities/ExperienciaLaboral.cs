1a50a09369126f34e5892125b758b8dc783edeb2
Carlos Diaz <carlos.cedp@gmail.com>
2022-02-13 20:36:32 -0400

    gestion de experiencia laboral: formulario creacion agregado

--- RecruitmentSystem.Domain/Entities/ExperienciaLaboral.cs
+++ RecruitmentSystem.Domain/Entities/ExperienciaLaboral.cs
@@ -11,12 +11,14 @@ namespace RecruitmentSystem.Domain.Entities
     public class ExperienciaLaboral: BaseEntity
     {
         public string Empresa { get; set; }
+        [Display(Name = "Puesto Ocupado")]
         public string PuestoOcupado { get; set; }
         [DataType(DataType.Date)]
         public DateTime FechaDesde { get; set; }
         [DataType(DataType.Date)]
         public DateTime FechaHasta { get; set; }
         public double Salario { get; set; }
+        [Display(Name = "Candidato")]
         public int CandidatoId { get; set; }
         public virtual Candidato Candidato { get; set; }
     }
---
+++ RecruitmentSystem.UI/Controllers/ExperienciaLaboralController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RecruitmentSystem.Domain.Abstract;
+using RecruitmentSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecruitmentSystem.UI.Controllers
+{
+    public class ExperienciaLaboralController : Controller
+    {
+        private readonly IExperienciaLaboralRepository _repository;
+        private readonly ICandidatoRepository _candidatoRepository;
+        public ExperienciaLaboralController(IExperienciaLaboralRepository repository, ICandidatoRepository candidatoRepository)
+        {
+            _repository = repository;
+            _candidatoRepository = candidatoRepository;
+        }
+        // GET: ExperienciaLaboralController
+        public ActionResult Index()
+        {
+            return View(_repository.GetAll().ToList());
+        }
+
+        // GET: ExperienciaLaboralController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ExperienciaLaboralController/Create
+        public ActionResult Create()
+        {
+            ViewBag.CandidatoId = new SelectList(_candidatoRepository.GetAll().ToList(), "Id", "Nombre");
+            return View();
+        }
+
+        // POST: ExperienciaLaboralController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ExperienciaLaboral _object)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _repository.Create(_object);
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ViewBag.CandidatoId = new SelectList(_candidatoRepository.GetAll().ToList(), "Id", "Nombre");
+                    return View();
+                }
+            }
+            catch (Exception e)
+            {
+                ViewBag.ErrorMessage = e.Message;
+                ViewBag.CandidatoId = new SelectList(_candidatoRepository.GetAll().ToList(), "Id", "Nombre");
+                return View();
+            }
+        }
+
+        // GET: ExperienciaLaboralController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ExperienciaLaboralController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ExperienciaLaboralController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ExperienciaLaboralController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
