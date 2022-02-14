ef39474af69512cd8ef7f1b0f9f215aaf95b913f
Carlos Diaz <carlos.cedp@gmail.com>
2022-02-14 18:46:49 -0400

    modelo departamento agregado: prueba de commit admin

---
+++ RecruitmentSystem.Domain/Entities/Departamento.cs
@@ -0,0 +1,16 @@
+﻿using RecruitmentSystem.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecruitmentSystem.Domain.Entities
+{
+    public class Departamento: BaseEntity
+    {
+        [Display(Name = "Departamento")]
+        public string Descripcion { get; set; }
+    }
+}
