b58fdc3b753158978e58dcb18a6b69e0f886ac8e
Carlos Diaz <carlos.cedp@gmail.com>
2022-02-16 17:00:36 -0400

    agregar context para identity y modlo personalizado, capa infraestructura

---
+++ RecruitmentSystem.Domain/Entities/UsuarioIdentity.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecruitmentSystem.Domain.Entities
+{
+    public class UsuarioIdentity: IdentityUser
+    {
+        //[PersonalData]
+        [StringLength(100)]
+        public string Firstname { get; set; }
+        //[PersonalData]
+        [StringLength(100)]
+        public string LastName { get; set; }
+    }
+}
---
+++ RecruitmentSystem.Infrastructure/Data/AuthDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecruitmentSystem.Domain.Entities;
+
+namespace RecruitmentSystem.Infrastructure.Data
+{
+    public class AuthDbContext: IdentityDbContext<UsuarioIdentity>
+    {
+        public AuthDbContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // SETUP CONNECTION STRING
+            optionsBuilder
+                .UseSqlServer("Server=(local);Database=RHSystemDB;trusted_connection=true");
+        }
+    }
+}
