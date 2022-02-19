b562886fce4a1c46c7e623c9d232d72db1e0db2f
Carlos Diaz <carlos.cedp@gmail.com>
2022-02-19 14:45:35 -0400

    editar empleado: agregado

--- RecruitmentSystem.UI/Controllers/EmpleadoController.cs
+++ RecruitmentSystem.UI/Controllers/EmpleadoController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using RecruitmentSystem.Domain.Abstract;
+using RecruitmentSystem.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +13,22 @@ namespace RecruitmentSystem.UI.Controllers
     public class EmpleadoController : Controller
     {
         private readonly IEmpleadoRepository _repository;
-        public EmpleadoController(IEmpleadoRepository repository)
+        private readonly IPuestoRepository _puestoRepository;
+        private readonly IDepartamentoRepository _departamentoRepository;
+        public EmpleadoController(
+            IEmpleadoRepository repository, 
+            IPuestoRepository puestoRepository,
+            IDepartamentoRepository departamentoRepository)
         {
+            _puestoRepository = puestoRepository;
             _repository = repository;
+            _departamentoRepository = departamentoRepository;
+        }
+
+        private void FillDropdownLists()
+        {
+            ViewBag.PuestoId = new SelectList(_puestoRepository.GetAll().ToList(), "Id", "Nombre");
+            ViewBag.DepartamentoId = new SelectList(_departamentoRepository.GetAll().ToList(), "Id", "Descripcion");
         }
         // GET: EmpleadoController
         public ActionResult Index()
@@ -63,20 +78,52 @@ namespace RecruitmentSystem.UI.Controllers
         // GET: EmpleadoController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            try
+            {
+                var data = _repository.GetById(id);
+                if (data == null)
+                    throw new Exception("Registro no encontrado");
+                FillDropdownLists();
+                return View(data);
+            }
+            catch (Exception e)
+            {
+                ViewBag.ErrorMessage = e.Message;
+                FillDropdownLists();
+                return View();
+            }
         }
 
         // POST: EmpleadoController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Empleado _object)
         {
             try
             {
+                if (!string.IsNullOrWhiteSpace(_object.Cedula)
+                    && _repository.ExisteEmpleadoCedula(_object.Cedula))
+                {
+                    ModelState.AddModelError("", "La cedula ingresada ya existe");
+                }
+
+                if (ModelState.IsValid)
+                {
+                    if (!_repository.CheckIfExists(id))
+                        throw new Exception("Registro no encontrado");
+                    _repository.Update(_object);
+                }
+                else
+                {
+                    FillDropdownLists();
+                    return View();
+                }
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception e)
             {
+                ViewBag.ErrorMessage = e.Message;
+                FillDropdownLists();
                 return View();
             }
         }
